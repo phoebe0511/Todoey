37c65439ab9347a347c9ab8bbd815c8b0b8847af
Phoebe Lin <phoebe.hp.lin@gmail.com>
2018-03-06 15:34:44 +0800

    Add todoey item

--- Todoey/Control/TodoListViewController.swift
+++ Todoey/Control/TodoListViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-    let itemArray = ["Eat launch🍱", "領錢", "喝飲料🎊"]
+    var itemArray = ["Eat launch🍱", "領錢", "喝飲料🎊"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +20,8 @@ class TodoListViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: - TableView Delegate Methods
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
@@ -34,6 +36,23 @@ class TodoListViewController: UITableViewController {
         print(itemArray[indexPath.row])
         tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    @IBAction func btnPressedAdd(_ sender: UIBarButtonItem) {
+        var textFiled = UITextField()
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (alertaction) in
+            if let textSrt = textFiled.text {
+                self.itemArray.append(textSrt)
+                self.tableView.reloadData()
+            }
+        }
+        alert.addAction(action)
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textFiled = alertTextField
+        }
+        present(alert, animated: true, completion: nil)
     }
 }
 
